ec1247a6ee65d05055a8035e16e73616a4db4134
KarimElhakim <karimali1896@gmail.com>
2025-07-18 05:53:10 +0300

    feat: connect to MongoDB via Mongoose and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Load .env into process.env globally
+    ConfigModule.forRoot({ isGlobal: true }),
+    // Connect to MongoDB using the URI from .env
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
